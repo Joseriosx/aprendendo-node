d3e5e6388cdc2af1f1fed6ede18dda8c71dabe24
joseriosx <joseriosjr03@gmail.com>
2026-04-28 17:15:43 -0300

    Exercicio 11

---
+++ compras.ts
@@ -0,0 +1,7 @@
+let frutas:string[] = ['Maça', 'Pera', 'Uva', 'Melancia']
+
+//Interação da lista
+for(let i = 0; i < frutas.length; i++) {
+        let contador = i + 1
+        console.log(contador + '. ' + frutas[i])
+}
