aeabb5f9037fe7b25d80e63961fcc835ddf9c1cb
Sbralzinho <sbralzinho2008@gmail.com>
2026-04-29 14:41:30 -0300

    atividade contagem

--- contagem.ts
+++ contagem.ts
@@ -1,3 +1,13 @@
-for(let contador = 10; contador >= 0; contador--) {
-	console.log(contador)
+const readline = require('readline');
+
+console.log("iniciando contagem regressiva...");
+
+// a estrutura do for:
+// 1. let i = 10: começamos no 10 
+// 2. i >= 0: o laço continua enquanto i for maior ou igual a zero
+// 3. i--: subtraimos 1 a cada volta (decretamento)
+for (let i = 10; i >= 0; i--) {
+	console.log(i);
 }
+
+console.log("fim da contagem!");
